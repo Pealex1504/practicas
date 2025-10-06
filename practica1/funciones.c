68e24af4af07f78eec9e000d373021428c36acdd
Pealex1504 <alexis.ramirez.rivera@estudiantxs.uacm.edu.mx>
2025-10-05 20:48:07 -0600

    Implementada la funcion invertirArreglo

--- practica1/funciones.c
+++ practica1/funciones.c
@@ -18,7 +18,20 @@ int longitudCadena(char *cadena) {
 	}
 	return tamano; // Retorna el valor final de la longitud de la cadena.
 }
-void invertirArreglo (int *arr , int size);
+//La funcion invierte el orden de un arreglo de enteros.
+//El parametro  arr es puntero al inicio del arreglo.
+//El parametro size es el tamano del arreglo.
+void invertirArreglo(int *arr, int size) {
+	int *inicio = arr; // Declara un puntero "inicio" y lo asigna al primer elemento del arreglo.
+	int *fin = arr + size - 1; // Declara un puntero "fin" y lo asigna al ultimo elemento del arreglo.
+	while (inicio < fin) { // El bucle se ejecuta mientras el puntero "inicio" sea menor que el puntero "fin".
+		int temp = *inicio; // Declara una variable entera temporal "temp" y le asigna el valor del elemento de 'inicio'.
+		*inicio = *fin; // Se asigna el valor del elemento de 'fin' al lugar de memoria de 'inicio'.
+		*fin = temp; // Se asigna el valor de la variable temporal "temp" al lugar de memoria de 'fin'.
+		inicio++; // El puntero 'inicio' se mueve hacia adelante.
+		fin--; // El puntero 'fin' se mueve hacia atras.
+	}
+}
 int cuentaPares (int *arr , int size);
 
 void main(){
